051ac01dee33a17a540f56875674968cb5e77225
seungminRyu <56435483+seungminRyu@users.noreply.github.com>
2020-11-12 00:44:44 +0900

    1111 위장 완료

--- week2_hash/camouflage.py
+++ week2_hash/camouflage.py
@@ -1,16 +1,23 @@
 def solution(clothes):
     clothHash = {}
+    answer = 1
     
     for set in clothes:
         cloth = set[0]
         clothType = set[1]
         if(clothType in clothHash):
-            clothHash[clothType].append(cloth)
+            clothHash[clothType] += 1
         else:
             clothHash[clothType] = []
-            clothHash[clothType].append(cloth)
+            clothHash[clothType] = 2
+    
+    typeNum = list(clothHash.values())
 
-    print(clothHash)
+    for i in typeNum:
+        answer *= i
+    answer -= 1
+
+    print(answer)    
     
 testcase1 = [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]
 testcase2 = [["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]
