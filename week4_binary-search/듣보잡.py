2cb17707bf8fc831fd35dfe2992e5fe8ad9cae15
seungminRyu <56435483+seungminRyu@users.noreply.github.com>
2020-11-25 02:14:19 +0900

    1124 듣보잡 틀림

---
+++ week4_binary-search/듣보잡.py
@@ -0,0 +1,101 @@
+# 날짜: 2020/11/25
+# 문제: 듣보잡 - 1764
+# 문제 요약: 듣도 못한 사람, 보도 못한 사람 리스트가 주어질때 듣도 보도 못한 사람 찾기
+
+"""
+방법 - 1
+
+두 배열을 모두 정렬,
+이분 탐색을 구현하여 첫번째 배열을 돌며 target을 설정
+두번째 배열에서 이분탐색으로 target을 찾는다.
+
+방법 - 2
+
+해쉬를 활용하여 입력 받을때 마다 값을 1증가
+값이 1이상일 경우 출력
+"""
+
+def binarySearch(target, start, end, data):
+    if start > end:
+        return 0
+
+    mid = (start + end) // 2
+
+    if data[mid] == target:
+        return data[mid]
+    elif data[mid] > target:
+        end = mid - 1
+    else:
+        start = mid + 1        
+
+    return binarySearch(target, start, end, data)
+
+def solution():
+    arr = list(map(int, input().split(" ")))
+    n = arr[0]
+    m = arr[1]
+    nList = []
+    mList = []
+    START = 0
+    END = m-1
+    find = []
+
+    for i in range(n):
+        nList.append(input())
+
+    for i in range(m):
+        mList.append(input())
+    mList.sort()
+
+    for target in nList:
+        result = binarySearch(target, START, END, mList)
+        if not (result == False):
+            find.append(result)
+
+    print(len(find))
+    for a in find:
+        print(a)
+
+solution()
+def binarySearch(target, start, end, data):
+    if start > end:
+        return 0
+
+    mid = (start + end) // 2
+
+    if data[mid] == target:
+        return data[mid]
+    elif data[mid] > target:
+        end = mid - 1
+    else:
+        start = mid + 1        
+
+    return binarySearch(target, start, end, data)
+
+def solution():
+    arr = list(map(int, input().split(" ")))
+    n = arr[0]
+    m = arr[1]
+    nList = []
+    mList = []
+    START = 0
+    END = m-1
+    find = []
+
+    for i in range(n):
+        nList.append(input())
+
+    for i in range(m):
+        mList.append(input())
+    mList.sort()
+
+    for target in nList:
+        result = binarySearch(target, START, END, mList)
+        if not (result == False):
+            find.append(result)
+
+    print(len(find))
+    for a in find:
+        print(a)
+
+solution()
--- week4_binary-search/수 찾기.py
+++ week4_binary-search/수 찾기.py
@@ -2,7 +2,8 @@
 # 문제: 수 찾기 - 1920
 # 문제 요약: 정수 배열을 받고 또 다른 정수 배열을 한 개 더 받는다. 두번째 배열에 있는 수들이 첫번째 배열에 있는지를 조사한다.
 
-def binary_search_recursion(target, start, end, data):
+# 이분탐색 구현
+def binarySearch(target, start, end, data):
     if start > end:
         return 0
 
@@ -15,17 +16,17 @@ def binary_search_recursion(target, start, end, data):
     else:
         start = mid + 1        
 
-    return binary_search_recursion(target, start, end, data)
+    return binarySearch(target, start, end, data)
 
 def solution():
     numsLen = int(input())
     numList = list(map(int, input().split(" ")))
     numList.sort()
     targetsLen = int(input())
-    targetList = list(map(int, list(input().split(" "))))
+    targetList = list(map(int, input().split(" ")))
 
     for target in targetList:
-        result = binary_search_recursion(target, 0, numsLen-1, numList)
+        result = binarySearch(target, 0, numsLen-1, numList)
         print(result)
 
 solution()
